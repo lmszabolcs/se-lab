83c43350e42abf69c6d16df288ee1ab08a645aa0
unknown <185074@BME.HU>
2024-10-16 09:22:41 +0200

    Add comments

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,7 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-
+  // Single instance of Random for class TorpedoStore
   static final Random RANDOM = new Random();
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
